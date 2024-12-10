2203d375ba0570f69da280ec4aa59d41fa024532
jadalikesfrogs <jada.smith@lethpolytech.ca>
2024-12-10 13:49:03 -0700

    uwu

--- main.cpp
+++ main.cpp
@@ -56,3 +56,87 @@ int main() {
 
     return 0;
 }
+//NEW SAMPLE 
+/*
+* 
+
+
+* Typewriter program from slide deck 6.2
+*
+* @file main.cpp
+* @author Stephen Graham
+* @date   2024-10-09
+* @brief  copy text from keyboard to file
+*
+*/
+//#include <iostream> // std::cin, std::cout
+//#include <fstream>  // std::ofstream
+//#include <string>   // std::string
+//using namespace std;
+
+/*
+* Open a file specified by the user and attach it to a stream object
+*
+* @return   none
+* @param[in,out]  ofstream& ofs is the stream to be opened
+*/
+//void open(ofstream& ofs);
+
+/*
+* Copy the console input to the output stream specified
+*
+* @return none
+* @param[in,out]  ofstream& ofs is the stream to be "fed"
+* @post           file attached to ofs has 0 or more characters inserted
+* @post           cin has had 0 or more characters removed
+
+void copyKeyboardTo(ofstream& ofs);
+
+int main() {
+    ofstream targetFile;
+
+    // open a user-specified file
+    open(targetFile);
+
+    // copy text until ~ character
+    copyKeyboardTo(targetFile);
+
+    // close
+    targetFile.close();
+
+    return 0;
+}
+
+void open(ofstream& ofs) {
+    // todo: what error checking do I need?
+    string filename;
+
+    // get a filename
+    cout << "Target file? ";
+    cin >> filename;
+
+    // what if the file already exists?samp
+    // what if the open operation fails?
+    // open the file
+    ofs.open(filename.c_str());
+
+    cout << "Opened the file" << endl;
+}
+
+void copyKeyboardTo(ofstream& ofs) {
+    // get a char from the console input
+    // while the current character is not ~ 
+        // put the character in the file
+        // get another char from console
+    char aCharacter = '\0';
+
+    cin.get(aCharacter);
+
+    while (aCharacter != '~' && !cin.fail() && !ofs.fail()) {
+        ofs.put(aCharacter);
+        cin.get(aCharacter);
+    }
+
+    cout << "Copied to the file" << endl;
+}
+*/
